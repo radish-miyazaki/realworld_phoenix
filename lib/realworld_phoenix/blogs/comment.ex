01be6079b916f727996d330794d7b0a1c22a9581
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-15 15:48:15 +0900

    add Comment relationships to Article

--- lib/realworld_phoenix/blogs/comment.ex
+++ lib/realworld_phoenix/blogs/comment.ex
@@ -2,9 +2,11 @@ defmodule RealworldPhoenix.Blogs.Comment do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias RealworldPhoenix.Blogs.Article
+
   schema "comments" do
     field :body, :string
-    field :article_id, :id
+    belongs_to :article, Article
 
     timestamps(type: :utc_datetime)
   end
@@ -12,7 +14,7 @@ defmodule RealworldPhoenix.Blogs.Comment do
   @doc false
   def changeset(comment, attrs) do
     comment
-    |> cast(attrs, [:body])
-    |> validate_required([:body])
+    |> cast(attrs, [:body, :article_id])
+    |> validate_required([:body, :article_id])
   end
 end
