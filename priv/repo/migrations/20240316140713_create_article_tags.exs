6e0dff872f5b5111c62e1b9f86a0b85e39bbcf0a
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-17 20:14:15 +0900

    add tags & article_tags migration

---
+++ priv/repo/migrations/20240316140713_create_article_tags.exs
@@ -0,0 +1,13 @@
+defmodule RealworldPhoenix.Repo.Migrations.CreateArticleTags do
+  use Ecto.Migration
+
+  def change do
+    create table(:article_tags, primary_key: false) do
+      add :article_id, references(:articles, on_delete: :nothing), null: false
+      add :tag_id, references(:tags, on_delete: :nothing), null: false
+    end
+
+    create index(:article_tags, [:article_id])
+    create index(:article_tags, [:tag_id])
+  end
+end
