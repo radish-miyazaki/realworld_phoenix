f3c1006e3c63d73aa61227ba7b38e93b1bd4d8ba
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2024-03-18 18:27:12 +0900

    add edit & delete button & func

--- lib/realworld_phoenix_web/live/article_live/show.ex
+++ lib/realworld_phoenix_web/live/article_live/show.ex
@@ -10,10 +10,29 @@ defmodule RealworldPhoenixWeb.ArticleLive.Show do
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
-    {:noreply,
-     socket
-     |> assign(:page_title, page_title(socket.assigns.live_action))
-     |> assign(:article, Blogs.get_article!(id))}
+    article = Blogs.get_article!(id)
+    %{live_action: action, current_user: user} = socket.assigns
+
+    if action == :edit && article.author.id != user.id do
+      {:noreply, push_navigate(socket, to: ~p"/articles/#{article}")}
+    else
+      {:noreply,
+       socket
+       |> assign(:page_title, page_title(socket.assigns.live_action))
+       |> assign(:article, Blogs.get_article!(id))}
+    end
+  end
+
+  @impl true
+  def handle_event("delete", _params, socket) do
+    %{article: article, current_user: user} = socket.assigns
+
+    if article.author.id != user.id do
+      {:noreply, socket}
+    else
+      {:ok, _} = Blogs.delete_article(article)
+      {:noreply, push_navigate(socket, to: ~p"/articles")}
+    end
   end
 
   defp page_title(:show), do: "Show Article"
